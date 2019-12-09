406d3d4dc9496e098816fb5c5f8756053791e900
hun-soso <lsh2077@naver.com>
2019-12-09 11:21:26 +0900

    19.12.06 mysql

---
+++ dml_ddl_blob/blob_query.py
@@ -0,0 +1,65 @@
+from DML_study.connection_pool import Connection_Pool
+from dml_ddl_blob.blob_read_write import read_file_blob, write_file_blob
+from mysql.connector import Error
+
+
+def insert_blob(query,file_name,file_path):
+    data =read_file_blob(file_path)
+    args= (file_name,data)
+    try:
+        conn = Connection_Pool.get_instance().get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def update_blob(query,file_name,file_path,no):
+    data =read_file_blob(file_path)
+    args= (file_name,data,no)
+    try:
+        conn = Connection_Pool.get_instance().get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+#select
+def read_blob(query,no,filename):
+    try:
+        conn = Connection_Pool.get_instance().get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, (no,))
+        photo=cursor.fetchone()[0]
+        write_file_blob(photo,filename)
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def delete_blob(sql,no):
+    try:
+        conn = Connection_Pool.get_instance().get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql,(no,))
+        conn.commit()
+    except Error as error:
+        print(error)
+
+    finally:
+        cursor.close()
+        conn.close()
--- dml_ddl_blob/main_blob.py
+++ dml_ddl_blob/main_blob.py
@@ -1,3 +1,6 @@
+from DML_study.coffee_select import query_with_fetchall
+from dml_ddl_blob.blob_query import insert_blob, read_blob, update_blob, delete_blob
+from dml_ddl_blob.blob_read_write import read_file_blob
 from dml_ddl_blob.create_table_blob import create_table
 
 
@@ -11,3 +14,27 @@ from dml_ddl_blob.create_table_blob import create_table
 
 if __name__=="__main__":
     create_table()
+    data=read_file_blob("img/python-logo.png")
+    print(type(data))
+
+    insert_query = "insert into images (name,pic) values(%s,%s)"
+    update_query = "update images set name=%s, pic = %s where no= %s"
+    read_query= "select pic from images where no =%s"
+    delete_query ="delete from images where no = %s"
+    select_query = "select no, name from images"
+
+    insert_blob(insert_query,'python-logo','img/python-logo.png')
+    insert_blob(insert_query, 'google-logo', 'img/google-logo.png')
+    query_with_fetchall(select_query)
+    print()
+
+    read_blob(read_query,1,'img/read-python.png')
+    read_blob(read_query, 2, 'img/read-google.png')
+
+    update_blob(update_query,'pycharm-logo','img/pycharm-logo.png',1)
+    query_with_fetchall(select_query)
+    print()
+    read_blob(read_query,1,"img/read-pycharm.png")
+
+    delete_blob(delete_query,1)
+    query_with_fetchall(select_query)
